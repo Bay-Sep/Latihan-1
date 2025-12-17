b76d01349c6c862ae821f988e47379905282a74a
Bayu Septian <bayysep01@gmail.com>
2025-12-17 21:32:14 +0700

    Delete Day 12 directory
    clean branch Day-13

--- Day 12/auth.js
+++
@@ -1,8 +0,0 @@
-function authMiddleware(req, res, next) {
-  if (!req.session.user) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-  next();
-}
-
-module.exports = authMiddleware;
